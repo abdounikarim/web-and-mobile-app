27c354de33e82e3934c5ebdabae101bcca5016ee
abdounikarim <abdounikarim@gmail.com>
2020-08-21 16:03:02 +0200

    create first app with Vue

---
+++ assets/js/App.vue
@@ -0,0 +1,15 @@
+<style>
+    body {
+        background-color: #00BFFF;
+    }
+</style>
+
+<template>
+    <h1>Hello World</h1>
+</template>
+
+<script>
+export default {
+    name: 'App',
+}
+</script>
--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,15 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+import Vue from 'vue';
+import App from './App.vue';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+new Vue({
+    //router,
+    render: h => h(App)
+}).$mount('#app')
